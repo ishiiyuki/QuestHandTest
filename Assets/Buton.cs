192328a9c15ca350a187707099f8201512b4c8ae
yuki_ishii <mizuki.izuna@gmail.com>
2019-12-25 19:53:24 +0900

    鍵盤でæきた

---
+++ Assets/Buton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buton : MonoBehaviour
+{
+    [SerializeField]
+    private Transform downTransform;
+
+    private Vector3 basePos;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        basePos= this.transform.localPosition;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void  OnTriggerEnter(Collider  col) {
+       
+        //this.transform.position  = baseTransform.position;
+    }
+
+    void  OnTriggerStay(Collider  col) {
+        this.transform.localPosition  = downTransform.localPosition;
+
+    }
+
+    void  OnTriggerExit(Collider  col) {
+        
+        this.transform.localPosition = basePos;
+    }
+}
